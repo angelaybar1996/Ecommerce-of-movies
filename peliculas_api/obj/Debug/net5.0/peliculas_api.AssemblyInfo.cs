6787f9669f15fbedf8dbfccf9223e9c8ca089f24
Angel Leonardo Aybar <al_aybar@farmacity.com.ar>
2025-03-28 22:10:31 -0300

    implementando LoginContext y localStorage

--- peliculas_api/obj/Debug/net5.0/peliculas_api.AssemblyInfo.cs
+++ peliculas_api/obj/Debug/net5.0/peliculas_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("peliculas_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119e20b8a0bf85f239b692289e6333cfbbd77c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263d5514a28d964141320c48dee2b16c9a352b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("peliculas_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("peliculas_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
